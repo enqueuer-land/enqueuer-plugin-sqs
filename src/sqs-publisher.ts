baa4f7547db2001466b1bacf3a77569ddcace9bf
Guilherme Silva Corsino <guilherme.c.trd@c6bank.com>
2019-06-25 19:23:49 -0300

    updates to enqueuer 5.0.0

--- src/sqs-publisher.ts
+++ src/sqs-publisher.ts
@@ -1,6 +1,6 @@
-import {Logger, MainInstance, Publisher, InputPublisherModel, PublisherProtocol} from 'enqueuer';
+import { Logger, MainInstance, Publisher, InputPublisherModel, PublisherProtocol } from 'enqueuer';
 import * as AWS from 'aws-sdk';
-import {SendMessageRequest, SendMessageResult} from 'aws-sdk/clients/sqs';
+import { SendMessageRequest, SendMessageResult } from 'aws-sdk/clients/sqs';
 
 export class SqsPublisher extends Publisher {
 
@@ -34,7 +34,55 @@ export class SqsPublisher extends Publisher {
 export function entryPoint(mainInstance: MainInstance): void {
     const sqs = new PublisherProtocol('sqs',
         (publisherModel: InputPublisherModel) => new SqsPublisher(publisherModel),
-        {onPublished: ['ResponseMetadata', 'MD5OfMessageBody', 'MD5OfMessageAttributes', 'MessageId']})
+        {
+            homepage: 'https://github.com/enqueuer-land/enqueuer-plugin-sqs',
+            description: 'Publisher to handle AWS sqs messages',
+            libraryHomepage: 'https://www.npmjs.com/package/aws-sdk',
+            schema: {
+                attributes: {
+                    payload: {
+                        type: 'any'
+                    },
+                    awsConfiguration: {
+                        type: {
+                            endpoint: {
+                                type: 'string'
+                            },
+                            accessKeyId: {
+                                type: 'string'
+                            },
+                            secretAccessKey: {
+                                type: 'string'
+                            },
+                            region: {
+                                type: 'string'
+                            }
+                        }
+                    },
+                },
+                hooks: {
+                    onPublished: {
+                        description: 'Executed when message was published correctly.',
+                        arguments: {
+                            ResponseMetadata: {
+                                description: 'Contains the RequestId which, in turn, contains the UUID of the request.'
+                            },
+                            MD5OfMessageBody: {
+                                description: 'An MD5 digest of the non-URL-encoded message body string.'
+                            },
+                            MD5OfMessageAttributes: {
+                                description: 'An MD5 digest of the non-URL-encoded message attribute string. ' +
+                                'You can use this attribute to verify that Amazon SQS received the message correctly.'
+                            },
+                            MessageId: {
+                                description: 'A unique identifier for the message. A MessageIdis considered unique ' +
+                                'across all AWS accounts for an extended period of time.'
+                            }
+                        }
+                    }
+                }
+            }
+        })
         .setLibrary('aws-sdk');
     mainInstance.protocolManager.addProtocol(sqs);
 }
--- src/sqs-subscription.ts
+++ src/sqs-subscription.ts
@@ -1,6 +1,6 @@
-import {Logger, MainInstance, Subscription, InputSubscriptionModel, SubscriptionProtocol} from 'enqueuer';
+import { Logger, MainInstance, Subscription, InputSubscriptionModel, SubscriptionProtocol } from 'enqueuer';
 import * as AWS from 'aws-sdk';
-import {ReceiveMessageResult} from 'aws-sdk/clients/sqs';
+import { ReceiveMessageResult } from 'aws-sdk/clients/sqs';
 
 export class SqsSubscription extends Subscription {
 
@@ -12,7 +12,7 @@ export class SqsSubscription extends Subscription {
         this.sqs = new AWS.SQS(subscriptionModel.awsConfiguration);
     }
 
-    public receiveMessage(): Promise<any> {
+    public receiveMessage(): Promise<void> {
         return new Promise((resolve, reject) => {
             this.sqs.receiveMessage(this.messageParams, (err: AWS.AWSError, data: ReceiveMessageResult) => {
                 Logger.trace(`SQS got data: ${JSON.stringify(data)}`);
@@ -21,22 +21,75 @@ export class SqsSubscription extends Subscription {
                     return reject(err);
                 } else if (data.Messages && data.Messages.length > 0) {
                     Logger.debug('SQS got a message: ' + JSON.stringify(data.Messages[0]));
-                    return resolve(data.Messages[0]);
+                    this.executeHookEvent('onMessageReceived', data.Messages[0]);
+                    return resolve();
                 }
             });
         });
     }
 
-    public subscribe(): Promise<void> {
-        return Promise.resolve();
+    public async subscribe(): Promise<void> {
+        this.executeHookEvent('onSubscribed');
     }
-
 }
 
 export function entryPoint(mainInstance: MainInstance): void {
     const sqs = new SubscriptionProtocol('sqs',
         (subscriptionModel: InputSubscriptionModel) => new SqsSubscription(subscriptionModel),
-        {onMessageReceived: ['Body', 'MessageId', 'ReceiptHandle', 'MD5OfBody']})
+        {
+            homepage: 'https://github.com/enqueuer-land/enqueuer-plugin-sqs',
+            description: 'Subscription to handle AWS sqs messages',
+            libraryHomepage: 'https://www.npmjs.com/package/aws-sdk',
+            schema: {
+                attributes: {
+                    messageParams: {
+                        type: 'object'
+                    },
+                    awsConfiguration: {
+                        type: {
+                            endpoint: {
+                                type: 'string'
+                            },
+                            accessKeyId: {
+                                type: 'string'
+                            },
+                            secretAccessKey: {
+                                type: 'string'
+                            },
+                            region: {
+                                type: 'string'
+                            }
+                        }
+                    },
+                },
+                hooks: {
+                    onSubscribed: {
+                        arguments: {},
+                        description: 'Executed when subscription was subscribed.',
+                    },
+                    onMessageReceived: {
+                        description: 'Executed when message was published correctly.',
+                        arguments: {
+                            Body: {
+                                description: 'The message\'s contents (not URL-encoded).'
+                            },
+                            MD5OfBody: {
+                                description: 'An MD5 digest of the non-URL-encoded message body string.'
+                            },
+                            ReceiptHandle: {
+                                description: 'An identifier associated with the act of receiving the message.' +
+                                    'A new receipt handle is returned every time you receive a message. When deleting ' +
+                                    'a message, you provide the last received receipt handle to delete the message.'
+                            },
+                            MessageId: {
+                                description: 'A unique identifier for the message. A MessageIdis considered unique ' +
+                                    'across all AWS accounts for an extended period of time.'
+                            }
+                        }
+                    }
+                }
+            }
+        })
         .setLibrary('aws-sdk');
     mainInstance.protocolManager.addProtocol(sqs);
 }
